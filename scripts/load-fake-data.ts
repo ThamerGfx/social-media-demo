ea68ddaddb98ce4e592bae73be64d4a7b5e38008
GOUIDER <thamer.gouider@cgi.com>
2023-10-23 17:43:00 +0200

    db connection

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,5 +1,22 @@
-function loadFakeData(numUsers: number = 10) {
+import { Client } from "pg";
+import { loadEnvConfig } from "@next/env";
+
+// const projectDir = process.cwd();
+// loadEnvConfig(projectDir);
+
+async function loadFakeData(numUsers: number = 10) {
   console.log(`generating ${numUsers} users`);
+
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "strings_app",
+    password: "postgres",
+    port: 5432,
+  });
+
+  //   const res = await client.query("salect 1");
+  console.log("aaaa: ", await client.query("salect 1"));
 }
 
 loadFakeData();
