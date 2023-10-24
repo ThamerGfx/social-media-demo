a7089df1cd10242c7f00b1028fc49780d8c3d2a5
GOUIDER <thamer.gouider@cgi.com>
2023-10-24 11:04:35 +0200

    load admin user

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,21 +1,11 @@
-import { Client } from "pg";
-import { loadEnvConfig } from "@next/env";
 import { faker } from "@faker-js/faker";
 import bcrypt from "bcrypt";
-
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
+import { getClient } from "@/db";
 
 async function loadFakeData(numUsers: number = 10) {
   console.log(`executing load fake data. generating ${numUsers} users.`);
 
-  const client = new Client({
-    user: process.env.POSTGRES_USER,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_NAME,
-    password: process.env.POSTGRES_PASSWORD,
-    port: parseInt(process.env.POSTGRES_PORT!),
-  });
+  const client = await getClient();
 
   await client.connect();
   try {
@@ -23,7 +13,8 @@ async function loadFakeData(numUsers: number = 10) {
 
     for (let i = 0; i < numUsers; i++) {
       const saltRounds = 10;
-      const hash = await bcrypt.hash("Thaaamer", saltRounds);
+      const hash = await bcrypt.hash("strings123", saltRounds);
+
       await client.query(
         "insert into public.users (username, password, avatar) values ($1, $2, $3)",
         [faker.internet.userName(), hash, faker.image.avatar()]
