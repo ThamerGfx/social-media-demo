5f9381f4c267fa5dfa1c118ef23bfb63ae1619dc
GOUIDER <thamer.gouider@cgi.com>
2023-11-02 00:19:47 +0100

    following page ui

--- app/(private)/following/page.tsx
+++ app/(private)/following/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
 import React from "react";
+import FollowingContainer from "./following-container";
 
 export default function Following() {
-  return <div>Following</div>;
+  return (
+    <main>
+      <h2>Following</h2>
+      <FollowingContainer />
+    </main>
+  );
 }
