9fe96dc5cbefa6f9a00c06858de8f59b57f61566
GOUIDER <thamer.gouider@cgi.com>
2023-11-07 13:56:47 +0100

    search bar func

---
+++ app/(private)/search-bar.tsx
@@ -0,0 +1,33 @@
+import * as _ from "lodash";
+import React, { ChangeEvent, useState } from "react";
+
+export default function SearchBar() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const debouncedFetchSearchResults = _.debounce(fetchSearchResults, 500);
+
+  async function fetchSearchResults(searchText: string) {
+    const res = await fetch("/api/search?q=" + searchText);
+    if (res.ok) {
+      const json = await res.json();
+      console.log(json);
+
+      setSearchResults(json.data);
+    }
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    debouncedFetchSearchResults(e.target.value);
+  }
+
+  return (
+    <div>
+      <input
+        onChange={handleChange}
+        type="text"
+        className="p-2 rounded-lg bg-gray-700 my-2 text-white"
+        placeholder="Search"
+      />
+    </div>
+  );
+}
