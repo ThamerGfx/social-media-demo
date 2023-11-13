2135874e7879aa407cd2f11002374a655946161e
GOUIDER <thamer.gouider@cgi.com>
2023-11-13 11:02:21 +0100

    upload avatar with vercel blob

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "cloudflare-ipfs.com"],
+    domains: [
+      "avatars.githubusercontent.com",
+      "cloudflare-ipfs.com",
+      "iplpgvzabpsu1dss.public.blob.vercel-storage.com",
+    ],
   },
 };
 
